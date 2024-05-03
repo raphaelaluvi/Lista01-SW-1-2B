f3101c25539a6149c4a0884f6ae410a4497b07cf
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-03 11:54:14 -0300

    Add files via upload

---
+++ exe02/exe02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exe02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //criando a instancia da classe
+            RegistroDeCompras registro = new RegistroDeCompras();
+
+            // Adicionando algumas compras
+            //Compra novaCompra = new Compra { Data = data, Produto = produto, Valor = valor };
+            registro.AdicionarCompra(new DateTime(2024, 4, 25), "Camisa", 49.99);
+            registro.AdicionarCompra(new DateTime(2024, 4, 26), "Calça Jeans", 79.99);
+            registro.AdicionarCompra(new DateTime(2024, 4, 27), "Tênis", 99.99);
+
+            // ta chamando o metodo de listar as compras
+            registro.ListarCompras();
+        }
+    }
+}
---
+++ exe02/exe02/RegistroDeCompras.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe02
+{
+    class RegistroDeCompras
+    {
+        // aqui o struct esta definindo essa estrutura das compras
+        public struct Compra
+        {
+            public DateTime Data;
+            public string Produto;
+            public double Valor;
+        }
+
+        // vai armazenar as compras em uma lista
+        private List<Compra> compras;
+
+        // o construtor, tem o mesmo nome q a classe
+        public RegistroDeCompras()
+        {
+            // Inicializa a lista de compras
+            compras = new List<Compra>();
+        }
+
+        //esse metodo va adicionar uma nova compra
+        public void AdicionarCompra(DateTime data, string produto, double valor)
+        {
+            Compra novaCompra = new Compra { Data = data, Produto = produto, Valor = valor };
+            compras.Add(novaCompra);
+        }
+
+        //metodo que lista as compra registradas
+
+        public void ListarCompras()
+        {
+            if (compras.Count == 0)
+            {
+                Console.WriteLine("Não há compras registradas.");
+                return;
+            }
+
+
+            //foreach é um recurso do C# que possibilita executar um conjunto de comandos para cada elemento presente em uma lista
+            Console.WriteLine("Registro de Compras:");
+            foreach (var compra in compras)
+            {
+                Console.WriteLine($"Data: {compra.Data.ToShortDateString()}, Produto: {compra.Produto}, Valor: {compra.Valor}");
+            }
+        }
+    }
+
+}
