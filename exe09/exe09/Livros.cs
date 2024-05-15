06a34cd566d039a19bb081c17f49c9f7c316fa66
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-15 16:50:08 -0300

    Add files via upload

---
+++ exe09/exe09/Livros.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe09
+{
+    public class Livro
+    {
+        private string titulo;
+        public string Titulo
+        {
+            get { return titulo; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O título do livro não pode ser vazio.");
+                }
+                titulo = value;
+            }
+        }
+
+        private string autor;
+        public string Autor
+        {
+            get { return autor; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O autor do livro não pode ser vazio.");
+                }
+                autor = value;
+            }
+        }
+    }
+}
---
+++ exe09/exe09/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace exe09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria um livro
+            Livro livro = new Livro();
+
+            // ta tentando colocar o titulo vazio
+            try
+            {
+                livro.Titulo = "";
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            /*tenta atribuir um valor inválido à propriedade Autor do objeto livro, 
+             * e caso isso gere uma exceção, captura essa exceção e imprime a mensagem 
+             * associada a ela*/
+            try
+            {
+                livro.Autor = "";
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // agr valores q de certo
+            livro.Titulo = "Dom Quixote";
+            livro.Autor = "Miguel de Cervantes";
+
+            // mostrando as informaçoes
+            Console.WriteLine($"Livro: {livro.Titulo}, Autor: {livro.Autor}");
+        }
+    }
+}
