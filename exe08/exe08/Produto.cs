cfd4847970f09c2e750fa53184be83edbca6bd95
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-15 15:26:29 -0300

    Add files via upload

---
+++ exe08/exe08/Produto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe08
+{
+    public class Fabricante
+    {
+        private string nome;
+        public string Nome
+        {
+            //ao inves de ja colocar get e set, aqui acaba sendo assim por causa das excessoes
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    nome = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O nome do fabricante não pode ser vazio.");
+                }
+            }
+        }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+    }
+
+    public class Produto
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    nome = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O nome do produto não pode ser vazio.");
+                }
+            }
+        }
+        public Fabricante Fabricante { get; set; }
+        private decimal preco;
+        public decimal Preco
+        {
+            get { return preco; }
+            set
+            {
+                if (value >= 0)
+                {
+                    preco = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O preço do produto deve ser positivo.");
+                }
+            }
+        }
+    }
+}
---
+++ exe08/exe08/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exe08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //chama a class
+            Fabricante fabricante = new Fabricante
+            {
+                Nome = "Empresa XYZ",
+                Endereco = "Rua ABC, 123",
+                Cidade = "Cidade A"
+            };
+
+            Produto produto = new Produto
+            {
+                Nome = "Celular",
+                Fabricante = fabricante,
+                Preco = 999.99m
+            };
+
+            Console.WriteLine($"Produto: {produto.Nome}");
+            Console.WriteLine($"Fabricante: {produto.Fabricante.Nome}");
+            Console.WriteLine($"Preço: R${produto.Preco}");
+        }
+    }
+}
