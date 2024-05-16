b3b5ce51170c13730e143850d0bc49b96fa25bfa
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-16 19:04:13 -0300

    Add files via upload

---
+++ exe10/exe10/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe10
+{
+    public class Animal
+    {
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public int Idade { get; set; }
+
+        public void EmitirSom() 
+        {
+            Console.WriteLine($"O {Especie} {Nome} está emitindo um som");
+        }
+    }
+}
---
+++ exe10/exe10/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exe10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria animal
+            Animal animal = new Animal
+            {
+                Nome = "Rex",
+                Especie = "Cachorro",
+                Idade = 5
+            };
+
+            // puxa para emitir o som
+            animal.EmitirSom();
+        }
+    }
+}
