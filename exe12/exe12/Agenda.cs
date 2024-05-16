3f2e3f5551346828ec3dddf1c621d02b2aec4efa
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-16 19:30:46 -0300

    Add files via upload

---
+++ exe12/exe12/Agenda.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe12
+{
+    class Pessoa
+    {
+        //criando atributos
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public float Altura { get; set; }
+
+        //construtor para arrumar em variaveis o q recebemos
+        public Pessoa(string nome, int idade, float altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Altura = altura;
+        }
+
+        //esta transformando tudo para string, pq esta concatenando os atributos
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Idade: {Idade}, Altura: {Altura}";
+        }
+    }
+
+    class Agenda
+    {
+        //criando a lista chamando a classe pessoa
+        private List<Pessoa> pessoas = new List<Pessoa>();
+
+        //armazena os dados das pessoas e adiciona na lista
+        public void ArmazenaPessoa(string nome, int idade, float altura)
+        {
+            //criando um objeto chamando a class pessoa
+            Pessoa pessoa = new Pessoa(nome, idade, altura);
+            pessoas.Add(pessoa);
+        }
+
+        //remove a pessoa pelo nome
+        public void RemovePessoa(string nome)
+        {
+            Pessoa pessoa = BuscaPessoa(nome);
+            //se for diferente de num
+            if (pessoa != null)
+            {
+                //lista.Remove(objeto)
+                pessoas.Remove(pessoa);
+                Console.WriteLine($"Pessoa {nome} removida da agenda.");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa {nome} não encontrada na agenda.");
+            }
+        }
+
+        //metodo q busca a pessoa
+        public Pessoa BuscaPessoa(string nome)
+        {
+            //seria um percorra a lista pessoas para achar a pessoa da class Pessoa
+            foreach (Pessoa pessoa in pessoas)
+            {
+                //vai validar se o nome do objeto é igual ao nome que vao deletar
+                if (pessoa.Nome == nome)
+                {
+                    //se for sim, vai retornar o objeto q corresponde ao nome
+                    return pessoa;
+                }
+            }
+            //senao, estregue null
+            return null;
+        }
+
+        public void ImprimeAgenda()
+        {
+            //Count conta o comprimento da lista
+            if (pessoas.Count == 0)
+            {
+                Console.WriteLine("Agenda vazia.");
+            }
+            else
+            {
+                Console.WriteLine("Pessoas na agenda:");
+                //seria o for, nome da class, nome objeto in nome da lista
+                foreach (Pessoa pessoa in pessoas)
+                {
+                    Console.WriteLine(pessoa);
+                }
+            }
+        }
+    }
+}
---
+++ exe12/exe12/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace exe12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //chama a class
+            Agenda agenda = new Agenda();
+            string opcao;
+
+            do
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Adicionar Pessoa");
+                Console.WriteLine("2. Remover Pessoa");
+                Console.WriteLine("3. Buscar Pessoa");
+                Console.WriteLine("4. Imprimir Agenda");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha uma opção: ");
+                //armazena a opcao
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.Write("Nome: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("Idade: ");
+                        int idade = int.Parse(Console.ReadLine());
+                        Console.Write("Altura: ");
+                        float altura = float.Parse(Console.ReadLine());
+                        //chama o metodo q adiciona e passa os parametros
+                        agenda.ArmazenaPessoa(nome, idade, altura);
+                        break;
+
+                    case "2":
+                        Console.Write("Nome da pessoa a ser removida: ");
+                        string nomeRemover = Console.ReadLine();
+                        //chama o metodo q remove e passa o parametro do nome para remover q recebeu
+                        agenda.RemovePessoa(nomeRemover);
+                        break;
+
+                    case "3":
+                        Console.Write("Nome da pessoa a ser buscada: ");
+                        string nomeBuscar = Console.ReadLine();
+                        Pessoa pessoaEncontrada = agenda.BuscaPessoa(nomeBuscar);
+                        if (pessoaEncontrada != null)
+                        {
+                            Console.WriteLine("Pessoa encontrada: " + pessoaEncontrada);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pessoa não encontrada na agenda.");
+                        }
+                        break;
+
+                    case "4":
+                        agenda.ImprimeAgenda();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Saindo...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            } while (opcao != "5"); //enquanto a opcao for diferente de 5
+        }
+    }
+}
