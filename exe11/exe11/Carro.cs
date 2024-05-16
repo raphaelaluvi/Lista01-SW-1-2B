493efce04063bc3fbede033cc8d9976369cdbef8
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-16 19:14:13 -0300

    Add files via upload

---
+++ exe11/exe11/Carro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe11
+{
+    class Carro
+    {
+        // sao os atributos do objeto
+        public string modelo;
+        public int ano;
+        public int velocidade;
+
+        // esse é o construtor
+        public Carro(string modelo, int ano)
+        {
+            this.modelo = modelo;
+            this.ano = ano;
+            this.velocidade = 0; // quando um carro é criado a sua velocidade é 0
+        }
+
+        // acelera o carro
+        public void Acelerar()
+        {
+            velocidade += 10;
+        }
+
+        // freia o carro
+        public void Frear()
+        {
+            if (velocidade >= 10) // verifica a velocidade para nao ficar negativa
+            {
+                velocidade -= 10;
+            }
+            else
+            {
+                velocidade = 0; // se for menor coloca como 0
+            }
+        }
+    }
+}
---
+++ exe11/exe11/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exe11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro meuCarro = new("Fusca", 1970);
+
+            //chama o metodo q acelera
+            meuCarro.Acelerar();
+            meuCarro.Acelerar();
+
+            Console.WriteLine("Velocidade após acelerar: " + meuCarro.velocidade);
+
+            // vai frear o carro
+            meuCarro.Frear();
+
+            Console.WriteLine("Velocidade após frear: " + meuCarro.velocidade);
+        }
+    }
+}
