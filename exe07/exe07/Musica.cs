3988ca678d8305b09d2757e9aa8e627ca17a95d3
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-17 12:02:38 -0300

    Add files via upload

---
+++ exe07/exe07/Musica.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe07
+{
+    public class Musica
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Gravadora { get; set; }
+
+        public Musica(string nome, string autor, string gravadora)
+        {
+            Nome = nome;
+            Autor = autor;
+            Gravadora = gravadora;
+        }
+    }
+}
--- exe07/exe07/Playlist.cs
+++ exe07/exe07/Playlist.cs
@@ -6,20 +6,6 @@ using System.Threading.Tasks;
 
 namespace exe07
 {
-    public class Musica
-    {
-        public string Nome { get; set; }
-        public string Autor { get; set; }
-        public string Gravadora { get; set; }
-
-        public Musica(string nome, string autor, string gravadora)
-        {
-            Nome = nome;
-            Autor = autor;
-            Gravadora = gravadora;
-        }
-    }
-
     public class Playlist
     {
         private List<Musica> listaDeMusicas;
