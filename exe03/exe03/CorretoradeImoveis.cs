efc50adbdfc5fbaae230fc43da6eda58607993c1
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-17 11:47:24 -0300

    Add files via upload

--- exe03/exe03/CorretoradeImoveis.cs
+++ exe03/exe03/CorretoradeImoveis.cs
@@ -6,21 +6,6 @@ using System.Threading.Tasks;
 
 namespace exe03
 {
-    //criando o imovel
-    class Imovel
-    {
-        public string Endereco { get; set; }
-        public double Preco { get; set; }
-        public string Tipo { get; set; }
-
-        public Imovel(string endereco, double preco, string tipo)
-        {
-            Endereco = endereco;
-            Preco = preco;
-            Tipo = tipo;
-        }
-    }
-
     //construtor:
     class CorretoraDeImoveis
     {
---
+++ exe03/exe03/Imovel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+    //criando o imovel
+    class Imovel
+    {
+        public string Endereco { get; set; }
+        public double Preco { get; set; }
+        public string Tipo { get; set; }
+
+        public Imovel(string endereco, double preco, string tipo)
+        {
+            Endereco = endereco;
+            Preco = preco;
+            Tipo = tipo;
+        }
+    }
+}
