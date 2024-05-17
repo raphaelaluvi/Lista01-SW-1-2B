27dfccbebc8fe2a77a69e6544ac0e58e0eb3ab7b
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-17 12:08:03 -0300

    Add files via upload

--- exe12/exe12/Agenda.cs
+++ exe12/exe12/Agenda.cs
@@ -6,28 +6,6 @@ using System.Threading.Tasks;
 
 namespace exe12
 {
-    class Pessoa
-    {
-        //criando atributos
-        public string Nome { get; set; }
-        public int Idade { get; set; }
-        public float Altura { get; set; }
-
-        //construtor para arrumar em variaveis o q recebemos
-        public Pessoa(string nome, int idade, float altura)
-        {
-            Nome = nome;
-            Idade = idade;
-            Altura = altura;
-        }
-
-        //esta transformando tudo para string, pq esta concatenando os atributos
-        public override string ToString()
-        {
-            return $"Nome: {Nome}, Idade: {Idade}, Altura: {Altura}";
-        }
-    }
-
     class Agenda
     {
         //criando a lista chamando a classe pessoa
---
+++ exe12/exe12/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe12
+{
+    class Pessoa
+    {
+        //criando atributos
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public float Altura { get; set; }
+
+        //construtor para arrumar em variaveis o q recebemos
+        public Pessoa(string nome, int idade, float altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Altura = altura;
+        }
+
+        //esta transformando tudo para string, pq esta concatenando os atributos
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Idade: {Idade}, Altura: {Altura}";
+        }
+    }
+}
