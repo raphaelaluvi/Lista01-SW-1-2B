603e84fe1578e152cf09b45b70952fa2e3476934
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-17 12:05:40 -0300

    Add files via upload

---
+++ exe08/exe08/Fabricante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe08
+{
+    public class Fabricante
+    {
+        private string nome;
+        public string Nome
+        {
+            //ao inves de ja colocar get e set, aqui acaba sendo assim por causa das excessoes
+            get { return nome; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    nome = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O nome do fabricante não pode ser vazio.");
+                }
+            }
+        }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+    }
+}
--- exe08/exe08/Produto.cs
+++ exe08/exe08/Produto.cs
@@ -6,29 +6,6 @@ using System.Threading.Tasks;
 
 namespace exe08
 {
-    public class Fabricante
-    {
-        private string nome;
-        public string Nome
-        {
-            //ao inves de ja colocar get e set, aqui acaba sendo assim por causa das excessoes
-            get { return nome; }
-            set
-            {
-                if (!string.IsNullOrWhiteSpace(value))
-                {
-                    nome = value;
-                }
-                else
-                {
-                    throw new ArgumentException("O nome do fabricante não pode ser vazio.");
-                }
-            }
-        }
-        public string Endereco { get; set; }
-        public string Cidade { get; set; }
-    }
-
     public class Produto
     {
         private string nome;
