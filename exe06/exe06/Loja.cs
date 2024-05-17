a42b854a1cdefbfa073eebe639da45ec98163a57
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2024-05-17 11:59:58 -0300

    Add files via upload

---
+++ exe06/exe06/Loja.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe06
+{
+    class Loja
+    {
+        private List<Produto> produtos = new List<Produto>();
+
+        // Método para adicionar um produto à lista de produtos
+        public void AdicionarProduto(Produto produto)
+        {
+            produtos.Add(produto);
+        }
+
+        // Método para exibir todos os produtos na lista
+        public void ExibirProdutos()
+        {
+            Console.WriteLine("Produtos na loja:");
+            foreach (Produto produto in produtos)
+            {
+                produto.ExibirInformacoes();
+            }
+        }
+
+        // Método para consultar um produto pelo nome
+        public Produto ConsultarProduto(string nome)
+        {
+            foreach (Produto produto in produtos)
+            {
+                if (produto.Nome == nome)
+                {
+                    return produto;
+                }
+            }
+            return null;
+        }
+    }
+}
--- exe06/exe06/Produto.cs
+++ exe06/exe06/Produto.cs
@@ -46,39 +46,4 @@ namespace exe06
             Console.WriteLine($"Nome: {Nome}, Preço: {Preco:C}, Quantidade em estoque: {QuantidadeEmEstoque}");
         }
     }
-
-    class Loja
-    {
-        private List<Produto> produtos = new List<Produto>();
-
-        // Método para adicionar um produto à lista de produtos
-        public void AdicionarProduto(Produto produto)
-        {
-            produtos.Add(produto);
-        }
-
-        // Método para exibir todos os produtos na lista
-        public void ExibirProdutos()
-        {
-            Console.WriteLine("Produtos na loja:");
-            foreach (Produto produto in produtos)
-            {
-                produto.ExibirInformacoes();
-            }
-        }
-
-        // Método para consultar um produto pelo nome
-        public Produto ConsultarProduto(string nome)
-        {
-            foreach (Produto produto in produtos)
-            {
-                if (produto.Nome == nome)
-                {
-                    return produto;
-                }
-            }
-            return null;
-        }
-    }
-
 }
